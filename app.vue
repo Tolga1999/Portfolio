9db590fa75eb56e069b9ef6fca8e8d67de6927fe
Tolga1999 <tolgadoganer@outlook.com>
2023-11-21 23:35:37 +0100

    home screen done

--- app.vue
+++ app.vue
@@ -1,10 +1,51 @@
 <script>
-console.log("hello")
 </script>
 
 <template>
-  <h1>hello</h1>
+  <AppHeader />
+  
+  <section class="circle-h1-container">
+    <img class="circle-svg" src="/svg/circle1.svg" alt="homescreen circle">
+    <h1>Frontend Developer</h1>
+  </section>
+
+  <div class="scroll-down">
+    <img src="/svg/arrow-right-down.svg" alt="arrow right down">
+    <span class="scroll-down-text">Scroll down</span>
+  </div>
 </template>
 
-<styles>
-</styles>
+<style>
+.circle-h1-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.circle-svg {
+  flex-grow: 2;
+  width: 100%;
+  max-width: 35em;
+  transform: translateY(-1.5em);
+}
+
+h1 {
+  z-index: 1;
+  transform: translateY(-4.5em);
+}
+
+.scroll-down {
+  display: flex;
+  position: absolute;
+  align-items: center;
+  bottom: 0;
+  right: 0;
+  margin: 1.5em 0.75em;
+}
+
+.scroll-down-text {
+  font-size: 1.25em;
+  font-weight: 600;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="#home">Tolga</NuxtLink>
+            <NuxtLink to="#projects">Projects</NuxtLink>
+            <span>2023</span>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        padding-top: 1.5em;
+        padding-bottom: 1em;
+        border-bottom: 3px solid var(--c-border-bottom);
+    }
+
+    nav{
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.25em;
+    }
+
+    a{
+        text-decoration: none;
+        color: var(--c-text);
+        font-weight: 400;
+    }
+</style>
